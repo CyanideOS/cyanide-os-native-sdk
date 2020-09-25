12b42a2a53f4de5d10d4d8b391724fee3d99038d
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-09-25 16:56:42 +0530

    Fixed SCSS bugs

--- window/window.component.ts
+++ window/window.component.ts
@@ -1,14 +1,14 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { WindowInterface } from '../services/window-manager/interfaces';
-import { WindowsManagerService } from '../services/window-manager/window-manager.service';
+import { Component, Input, OnInit } from "@angular/core";
+import { WindowInterface } from "../../services/managers/window-manager/interfaces";
+import { WindowsManagerService } from "../../services/managers/window-manager/window-manager.service";
 
 @Component({
-  selector: 'Window',
-  templateUrl: './window.component.html',
-  styleUrls: ['./window.component.scss'],
+  selector: "Window",
+  templateUrl: "./window.component.html",
+  styleUrls: ["./window.component.scss"],
 })
 export class WindowComponent implements OnInit {
-  @Input('window')
+  @Input("window")
   window: WindowInterface;
 
   constructor(private windowsManagerService: WindowsManagerService) {}
