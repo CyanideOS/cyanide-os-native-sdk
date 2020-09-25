2a67c51046298710522076c584e1b0c98e2f5a09
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-09-25 17:02:41 +0530

    Moved Window Component to UI Repository

--- native-sdk.module.ts
+++ native-sdk.module.ts
@@ -1,22 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { DragDropModule } from '@angular/cdk/drag-drop';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { DragDropModule } from "@angular/cdk/drag-drop";
 
-import { WindowComponent } from './window/window.component';
-import { NativeButton } from './ui/button/button.component';
-import { NativeCheckbox } from './ui/checkbox/checkbox.component';
-import { TextboxComponent } from './ui/textbox/textbox.component';
-import { SafeUrlPipe } from './pipes/safe-url/safe-url.pipe';
+import { NativeButton } from "./ui/button/button.component";
+import { NativeCheckbox } from "./ui/checkbox/checkbox.component";
+import { TextboxComponent } from "./ui/textbox/textbox.component";
+import { SafeUrlPipe } from "./pipes/safe-url/safe-url.pipe";
 
 @NgModule({
-  declarations: [
-    WindowComponent,
-    NativeButton,
-    NativeCheckbox,
-    TextboxComponent,
-    SafeUrlPipe,
-  ],
-  exports: [WindowComponent, NativeButton, NativeCheckbox, TextboxComponent],
+  declarations: [NativeButton, NativeCheckbox, TextboxComponent, SafeUrlPipe],
+  exports: [NativeButton, NativeCheckbox, TextboxComponent],
   imports: [CommonModule, DragDropModule],
 })
 export class NativeSdkModule {}
