e61081283268d82147ea30f6c7b31f13af6ee1eb
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-09-26 15:17:26 +0530

    Added NativeCheckbox styling

--- ui/controls/checkbox/checkbox.component.ts
+++ ui/controls/checkbox/checkbox.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'NativeCheckbox',
-  templateUrl: './checkbox.component.html',
-  styleUrls: ['./checkbox.component.scss'],
+  selector: "NativeCheckbox",
+  templateUrl: "./checkbox.component.html",
+  styleUrls: ["./checkbox.component.scss"],
 })
 export class NativeCheckbox implements OnInit {
+  isChecked: boolean = false;
   constructor() {}
 
   ngOnInit(): void {}
